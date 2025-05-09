49fd8e5572b2a6b475f1bd325423c5b23ddbcedc
Adrien Schneider <schneider.adrien@outlook.com>
2025-05-09 23:28:59 +0200

    Fixes mobile view + loading elements

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function timeAgo(timestamp) {
+    let text = "Latest activity: "
     const units = [
         { label: "year", seconds: 31536000 },
         { label: "month", seconds: 2592000 },
@@ -11,13 +12,13 @@ function timeAgo(timestamp) {
     const secondsElapsed = Math.floor(timestamp / 1000);
   
     if (secondsElapsed < 60) {
-        return "less than a minute ago";
+        return text + "less than a minute ago";
     }
   
     for (const unit of units) {
         const interval = Math.floor(secondsElapsed / unit.seconds);
         if (interval >= 1) {
-            return `about ${interval} ${unit.label}${interval > 1 ? "s" : ""} ago`;
+            return text + `${interval} ${unit.label}${interval > 1 ? "s" : ""} ago`;
         }
     }
 }
@@ -31,36 +32,40 @@ async function fetchData() {
         document.getElementById('last-activity-timestamp').textContent = `${timeAgo(data.elapsedTimeSinceActivity_ms)}`;
         document.getElementById('track-name').href = data.link;
 
-        if (data["image"]) {
+        if (data.isplaying == true) {
             document.getElementById('album-cover').src = data.image;
+            const progressText = document.getElementById('progress-text');
+            const minutes = Math.floor(data.progress_ms / 60000);
+            const seconds = Math.floor((data.progress_ms % 60000) / 1000);
+            progressText.textContent = `${minutes}:${seconds.toString().padStart(2, '0')} / ${Math.floor(data.duration_ms / 60000)}:${Math.floor((data.duration_ms % 60000) / 1000).toString().padStart(2, '0')}`;
+            const artistContainer = document.getElementById('artist-names');
+            artistContainer.innerHTML = '';
+            data.artistNames.forEach((artist, index) => {
+                const artistLink = document.createElement('a');
+                artistLink.href = data.artistLinks[index];
+                artistLink.textContent = artist;
+                artistLink.target = '_blank';
+                artistLink.classList.add('text-lg', 'text-gray-600', 'hover:underline');
+                artistContainer.appendChild(artistLink);
+                if (index < data.artistNames.length - 1) {
+                    artistContainer.appendChild(document.createTextNode(', '));
+                }
+            });
+
+            const progressBar = document.getElementById('custom-progress');
+            if (data.duration_ms > 0) {
+                const percent = (data.progress_ms / data.duration_ms) * 100;
+                progressBar.style.width = `${percent}%`;
+            }
         } else {
             document.getElementById('album-cover').src = 'assets/spotify.svg';
+            document.getElementById('progress-text').textContent = '-:-- / -:--';
+            document.getElementById('artist-names').textContent = "n/A";
+            document.getElementById('custom-progress').style.width = `0%`;
         }
 
-        const artistContainer = document.getElementById('artist-names');
-        artistContainer.innerHTML = '';
-        data.artistNames.forEach((artist, index) => {
-            const artistLink = document.createElement('a');
-            artistLink.href = data.artistLinks[index];
-            artistLink.textContent = artist;
-            artistLink.target = '_blank';
-            artistLink.classList.add('text-lg', 'text-gray-600', 'hover:underline');
-            artistContainer.appendChild(artistLink);
-            if (index < data.artistNames.length - 1) {
-                artistContainer.appendChild(document.createTextNode(', '));
-            }
-        });
+        
 
-        const progressBar = document.getElementById('custom-progress');
-        if (data.duration_ms > 0) {
-            const percent = (data.progress_ms / data.duration_ms) * 100;
-            progressBar.style.width = `${percent}%`;
-        }
-
-        const progressText = document.getElementById('progress-text');
-        const minutes = Math.floor(data.progress_ms / 60000);
-        const seconds = Math.floor((data.progress_ms % 60000) / 1000);
-        progressText.textContent = `${minutes}:${seconds.toString().padStart(2, '0')} / ${Math.floor(data.duration_ms / 60000)}:${Math.floor((data.duration_ms % 60000) / 1000).toString().padStart(2, '0')}`;
 
         
 
